00bcc441a27ff1d69e3e6caf83850ebf376f38ee
yinghui-techup <169006258+yinghui-techup@users.noreply.github.com>
2024-05-09 07:17:39 +0000

    created script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+var age = 20
+age = 65
+console.log(age)
+
+var username = "preshant"
+var password = "techup"
+
+var isMale = false
+var isLoggedIn = false
+
+var blog_posts = ["Today was a good day", "The sky is blue", 
+"It's raining heavily now"]
+
+console.log(username);
+console.log(password);
+console.log(isMale);
+console.log(isLoggedIn);
+console.log(blog_posts);
+
+if (isMale == true) {
+    console.log("You are male")
+}
+
+if (isMale == true) {
+    console.log("You are male")
+} else {
+    console.log("You are not male")
+}
+
+while (age < 70) {
+    console.log("Your age is " + age );
+    age = age + 1;
+}
+
+
+function sayHello() {
+    console.log("Hello everyone");
+}
+
+sayHello();
+
+function buttonClick() {
+    alert("Button clicked!");
+}
